222508b6168ff03d715f840a34b8bdedc967d9af
FarzanRashid <promit666@gmail.com>
2024-07-12 23:56:46 +0600

    Update Solution

--- LeetCode/Number Complement.py
+++ LeetCode/Number Complement.py
@@ -1,20 +1,22 @@
 # Problem no - 476
 
-# Time Complexity O(log n)
+# Time Complexity O(n) where n is the number of bits required for num"
 
 class Solution(object):
     def findComplement(self, num):
-        """
-        :type num: int
-        :rtype: int
-        """
-        output = ""
+        if num == 0:
+            return 1
+
+        output = 0
+        bit_id = 0
 
         while num:
             last_bit = num & 1
-            if last_bit:
-                output += "0"
-            else:
-                output += "1"
+            if last_bit == 0:
+                num = num | 1
+                val = 1 << bit_id
+                output = output | val
+
             num = num >> 1
-        return int(output[::-1], 2)
+            bit_id += 1
+        return output
