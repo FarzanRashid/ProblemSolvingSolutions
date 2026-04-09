4ee987097c1053565e230c1c20b50fa4d04d0338
FarzanRashid <promit666@gmail.com>
2026-04-09 23:42:49 +0600

    Add solution

---
+++ LeetCode/Substrings of Size Three with Distinct Characters.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        output = 0
+        for i in range(len(s) - 2):
+            if s[i] != s[i + 1] and s[i] != s[i + 2] and s[i + 1] != s[i + 2]:
+                output += 1
+        return output
+
+# Time complexity = O(N)
+# Space complexity = O(1)
