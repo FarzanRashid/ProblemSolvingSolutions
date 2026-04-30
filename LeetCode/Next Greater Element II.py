aca722f23868c25f45e3783545816339a1a10713
FarzanRashid <promit666@gmail.com>
2026-04-30 23:17:59 +0600

    Add solution

---
+++ LeetCode/Next Greater Element II.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        stack = []
+        n = len(nums)
+        output = [-1] * n
+        for i in range(2 * n):
+            while stack and nums[stack[-1]] < nums[i % n]:
+                idx = stack.pop()
+                output[idx] = nums[i % n]
+            if i < n:
+                stack.append(i)
+        return output
+
+# Time complexity = O(n)
+# Space complexity = O(n)
